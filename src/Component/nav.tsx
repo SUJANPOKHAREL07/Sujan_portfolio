56e0e045518b94b7cf9d5c2465ff4d59ff671033
sujan07 <sujanpokharel704@gmail.com>
2025-05-17 01:02:01 +0545

    home section is updated

--- src/Component/nav.tsx
+++ src/Component/nav.tsx
@@ -6,27 +6,27 @@ function Nav() {
     <>
     <nav>
        <div className="flex bg-gray-800 w-full  ">
-         <h1 className='font-bold text-2xl py-4 text-white ml-7'>Sujan Pokharel</h1>
+         <h1 className='font-bold text-2xl py-4  text-white ml-17'>Sujan Pokharel</h1>
        <div className="ml-[30rem] space-x-8 py-5 font-semibold">
          <NavLink 
         to="/"
-        className={({isActive})=>isActive? "text-green-100" : "text-white"}
+        className={({isActive})=>isActive? "text-purple-300" : "text-white"}
         >Home</NavLink>
          <NavLink 
         to="/about"
-        className={({isActive})=>isActive? "text-green-200" : "text-white"}
+        className={({isActive})=>isActive? "text-purple-300" : "text-white"}
         >About</NavLink>
           <NavLink 
         to="/projects"
-        className={({isActive})=>isActive? "text-green-200" : "text-white"}
+        className={({isActive})=>isActive? "text-purple-300" : "text-white"}
         >Projects</NavLink>
           <NavLink 
         to="/contact"
-        className={({isActive})=>isActive? "text-green-200" : "text-white"}
+        className={({isActive})=>isActive? "text-purple-300" : "text-white"}
         >Contact</NavLink>
        </div>
        <div className="py-4 ml-40  text-white font-bold">
-        <button className='bg-green-300 w-20 rounded-md h-8 border-b-4 border-green-300 hover:border-green-300 hover:bg-green-100 hover:text-green-400 rounded-2xl'>Hire Me!</button> 
+        <button className='bg-purple-300 w-20 rounded-md h-8 border-b-4 border-purple-300 hover:border-purple-300 hover:bg-purple-100 hover:text-purple-400 rounded-2xl'>Hire Me!</button> 
        </div>
        </div>
     </nav>
--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,9 +1,57 @@
+import { FaArrowAltCircleDown } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
 
-const Home=()=>{
-    return(
-        <>
-        <h1>this is home page </h1>
-        </>
-    )
-}
-export default Home
+const Home = () => {
+  return (
+    <>
+      <div className="bg-gray-800 h-[567px] p-29 w-full">
+        <div className="flex gap-3 font-bold text-3xl text-white ">
+          <h1>Hello I'm </h1>
+          <label htmlFor="">
+            Sujan Pokharel-
+            <label className="text-2xl text-purple-300  font-bold">
+              <TypeAnimation
+                sequence={[
+                  " Frontend developer",
+                  120,
+                  " Backend developer",
+                  120,
+                ]}
+                wrapper="span"
+                speed={20}
+                className="text-purple-300"
+                repeat={Infinity}
+              />
+            </label>
+          </label>
+        </div>
+
+        <div className="text-white p-2 flex ">
+          <p className="w-[60rem]">
+            {" "}
+            Hi, I'm Sujan Pokharel, a 6th semester BCA student at Crimson
+            College of Technology, affiliated with Pokhara University. I'm
+            currently a MERN stack development intern, where I’m gaining
+            practical experience in building full-stack web applications using
+            MongoDB, Express.js, React, and Node.js. I'm passionate about
+            coding, problem-solving, and creating user-friendly digital
+            solutions. I enjoy learning new technologies and working on
+            real-world projects that challenge me to grow as a developer.
+          </p>
+
+          <div className="  ">
+            <img
+              className="h-69 w-75 rounded-l-full rounded-r-full ml-20 my-[-5rem]                                      8 "
+              src="public\profile.png"
+              alt="profile-pic"
+            />
+          </div>
+        </div>
+        <button className=" flex ml-9  font-bold text-white bg-purple-300 w-fit  rounded-md h-8 border-b-4 border-purple-300 hover:border-purple-300 hover:bg-purple-100 hover:text-purple-400 rounded-2xl \">
+          Download Cv <FaArrowAltCircleDown className="mt-[5px] ml-1" />
+        </button>
+      </div>
+    </>
+  );
+};
+export default Home;
